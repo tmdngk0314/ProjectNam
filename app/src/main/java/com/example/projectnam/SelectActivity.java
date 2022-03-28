37a7962bb042535e369c6414cf05b2f10e389c93
Lee dong hee <tmdngk0314@naver.com>
2022-03-28 20:03:27 +0900

    reserve 버튼에 클릭리스너 추가

--- app/src/main/java/com/example/projectnam/SelectActivity.java
+++ app/src/main/java/com/example/projectnam/SelectActivity.java
@@ -9,14 +9,24 @@ import android.widget.ImageButton;
 import android.widget.RelativeLayout;
 
 public class SelectActivity extends AppCompatActivity {
-    ImageButton imgBtnLogout;
+    ImageButton imgBtnLogout, imgBtnreserve;
     RelativeLayout thirdRela;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select);
         imgBtnLogout = (ImageButton)findViewById(R.id.imgBtnLogout);
+        imgBtnreserve =(ImageButton)findViewById(R.id.storeimg) ;
         thirdRela = (RelativeLayout)findViewById(R.id.thirdRela);
+
+        imgBtnreserve.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SelectActivity.this, Reserve.class);
+                startActivity(intent);
+                finish();
+            }
+        });
         imgBtnLogout.setOnClickListener(new View.OnClickListener(){
             public void onClick(View view){
                 Intent intent = new Intent(SelectActivity.this, MainActivity.class);
