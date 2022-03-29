b85838269bbe619b3d1c9ddb0f797e81c63d6846
SungWoo <rjk0126@naver.com>
2022-03-29 15:10:21 +0900

    [FIX]선택 버튼 배경 변경, 폰트 변경(activity_select)

--- app/src/main/java/com/example/projectnam/NoticeActivity.java
+++ app/src/main/java/com/example/projectnam/NoticeActivity.java
@@ -1,24 +1,39 @@
 package com.example.projectnam;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.RelativeLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class NoticeActivity extends AppCompatActivity {
     ImageButton imgBtnLogout;
+    RelativeLayout relative1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notice);
+
         imgBtnLogout = (ImageButton) findViewById(R.id.imgBtnLogout);
+        relative1 = (RelativeLayout)findViewById(R.id.relative1);
+
+
 
         imgBtnLogout.setOnClickListener(new View.OnClickListener(){
             public void onClick(View view){
                 finish();
             }
         });
+
+        relative1.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view){
+                Intent intent = new Intent(NoticeActivity.this, NoticeInformationActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 
 }
