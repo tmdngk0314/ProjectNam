e8988ebd30474d9cdc445b172bc507cba33bd04c
SungWoo <rjk0126@naver.com>
2022-03-30 15:47:10 +0900

    [FIX]공지사항 이미지 전체 변경

--- app/src/main/java/com/example/projectnam/NoticeActivity.java
+++ app/src/main/java/com/example/projectnam/NoticeActivity.java
@@ -3,37 +3,27 @@ package com.example.projectnam;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ImageButton;
+import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class NoticeActivity extends AppCompatActivity {
-    ImageButton imgBtnLogout;
-    RelativeLayout relative1;
+
+    RelativeLayout notice_relative1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notice);
 
-        imgBtnLogout = (ImageButton) findViewById(R.id.imgBtnLogout);
-        relative1 = (RelativeLayout)findViewById(R.id.relative1);
+        notice_relative1 = (RelativeLayout) findViewById(R.id.notice_relative1);
 
-
-
-        imgBtnLogout.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View view){
-                finish();
-            }
-        });
-
-        relative1.setOnClickListener(new View.OnClickListener(){
+        notice_relative1.setOnClickListener(new View.OnClickListener(){
             public void onClick(View view){
                 Intent intent = new Intent(NoticeActivity.this, NoticeInformationActivity.class);
                 startActivity(intent);
             }
         });
-
     }
 
 }
