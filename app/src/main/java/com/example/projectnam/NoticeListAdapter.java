f0053f105be7e2027c1295f3a10caad8a04f91ea
동희 <tmdngk0314>
2022-04-01 19:32:02 +0900

    [ADD] 공지사항기능 코딩

---
+++ app/src/main/java/com/example/projectnam/NoticeListAdapter.java
@@ -0,0 +1,50 @@
+package com.example.projectnam;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class NoticeListAdapter extends BaseAdapter {
+
+    private Context context;
+    private List<Notice>  noticeList;
+
+    public NoticeListAdapter(Context context, List<Notice> noticeList) {
+        this.context = context;
+        this.noticeList = noticeList;
+    }
+
+    @Override
+    public int getCount() {
+        return noticeList.size();
+    }
+
+    @Override
+    public Object getItem(int i) {   //해당 위치에 있는 공지사항 반환
+        return noticeList.get(i);
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return i;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        View v = View.inflate(context, R.layout.notice_shape,null);
+        TextView noticeText = (TextView) v.findViewById(R.id.noticetext);
+        TextView dateText = (TextView) v.findViewById(R.id.dateText);
+
+        noticeText.setText(noticeList.get(i).getNotice());
+        dateText.setText(noticeList.get(i).getDate());
+
+        v.setTag(noticeList.get(i).getNotice());    //태그를 붙여줌
+        return v;
+
+    }
+
+}
