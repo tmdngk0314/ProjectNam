dcf8c932df5ef4d07e7dfe099537fb0fe212c576
SungWoo <rjk0126@naver.com>
2022-04-04 19:40:55 +0900

    [ADD] db에서 공지사항 가져오기 추가

--- app/src/main/java/com/example/projectnam/NoticeActivity.java
+++ app/src/main/java/com/example/projectnam/NoticeActivity.java
@@ -2,6 +2,7 @@ package com.example.projectnam;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.ListView;
@@ -9,10 +10,18 @@ import android.widget.RelativeLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class NoticeActivity extends AppCompatActivity {
+
+    int noticeCount=0;
+    int pageValue;
+
+
     private ListView noticeListView;
     private NoticeListAdapter adapter;
     private List<Notice> noticeList;
@@ -38,6 +47,45 @@ public class NoticeActivity extends AppCompatActivity {
         adapter = new NoticeListAdapter(getApplicationContext(), noticeList);
         noticeListView.setAdapter(adapter);  //리스트 뷰에 해당 어뎁터 매칭
 
+        CallRestApi apiCaller = new CallRestApi();
+        apiCaller.loadNotice(1);
+        //notice/load
+
+
+        /*
+        try {
+            notice.getRestAPI("notice/loadcount");
+            noticeCount = notice.receivedJSON.getInt("maxindex");
+            Log.i("loadCount ", Integer.toString(noticeCount));
+
+
+
+            String asd[][] = new String[noticeCount][4];
+            JSONObject page = new JSONObject();
+            //notice.postRestAPI(new JSONObject(Integer.toString(1)),"notice/load");
+            pageValue=1;
+
+            page.put("page", pageValue);
+
+            notice.postRestAPI(page,"notice/load");
+            for(int i=0;i<noticeCount;i++){
+                asd[i][0] = Integer.toString(noticeCount-i - (pageValue-1)*10);
+                asd[i][1] = notice.receivedJSON.getString("body");
+                asd[i][2] = notice.receivedJSON.getString("date");
+                asd[i][3] = notice.receivedJSON.getString("title");
+                Log.i("INDEX : ",asd[i][0]);
+                Log.i("BODY : ",asd[i][1]);
+                Log.i("DATE : ",asd[i][2]);
+                Log.i("TITLE : ",asd[i][3]);
+            }
+
+            // title body date index
+
+            Log.i("Heool","Hello22222222222222");
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }*/
 
 
     }
