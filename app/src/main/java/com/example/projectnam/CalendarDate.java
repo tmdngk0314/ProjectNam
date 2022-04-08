23bfe67128238bda441aeb2d3534358c81c939f7
SungWoo <rjk0126@naver.com>
2022-04-08 23:49:27 +0900

    [ADD] 캘린더뷰 시작 및 종료에 따른 선택 기능 추가

--- app/src/main/java/com/example/projectnam/CalendarDate.java
+++ app/src/main/java/com/example/projectnam/CalendarDate.java
@@ -1,13 +1,24 @@
 package com.example.projectnam;
 
+import android.icu.util.Calendar;
+import android.util.Log;
 import android.widget.CalendarView;
 
 import androidx.annotation.NonNull;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 
-public class CalendarDate implements CalendarView.OnDateChangeListener{
-    int Year = LocalDate.now().getYear(), Month = LocalDate.MAX.getMonthValue(), Day = LocalDate.now().getDayOfMonth();
+public class CalendarDate implements CalendarView.OnDateChangeListener {
+    int Year = LocalDate.now().getYear(), Month = LocalDate.now().getMonthValue(), Day = LocalDate.now().getDayOfMonth();
+    int finishYear = 2033, finishMonth = 12, finishDay = 31;
+    int startYear = Year, startMonth = Month, startDay = Day;
+    Long tim, now = System.currentTimeMillis();
+    CalendarView calendar;
+    SimpleDateFormat finish = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+    Calendar ca;
+    int offset = 0;
 
     @Override
     public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
@@ -15,4 +26,57 @@ public class CalendarDate implements CalendarView.OnDateChangeListener{
         Month = month + 1;
         Day = dayOfMonth;
     }
+
+    public void setCalendar(CalendarView ca) {
+        this.calendar = ca;
+    }
+
+    public void setMin() {
+        calendar.setMinDate(0);
+        try {
+            tim = finish.parse(Integer.toString(startYear) + "-" + Integer.toString(startMonth) + "-" + Integer.toString(startDay) + " 01:12:34").getTime();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        if (offset == 1){
+            calendar.setMinDate(now);
+            calendar.setDate(tim);
+        }
+        else {
+            calendar.setMinDate(tim);
+        }
+    }
+    public void setOffset(int offset) {
+        this.offset = offset;
+    }
+
+    public void setMax() {
+        //9999년 12월 31일 값으로 최대값을 늘린 후 줄일 수 있다.
+        calendar.setMaxDate(253402186354000L);
+        try {
+                tim = finish.parse(Integer.toString(finishYear) + "-" + Integer.toString(finishMonth) + "-" + Integer.toString(finishDay) + " 01:12:34").getTime();
+            } catch (ParseException e) {
+                e.printStackTrace();
+        }
+
+        if (offset == 1) calendar.setMaxDate(tim);
+
+        else {
+            if(tim==2019571954000L) calendar.setDate(now);
+            else calendar.setDate(tim);
+        }
+
+    }
+
+    public void saveFinish() {
+        finishYear = Year;
+        finishMonth = Month;
+        finishDay = Day;
+    }
+    public void saveStart(){
+        startYear = Year;
+        startMonth = Month;
+        startDay = Day;
+    }
+
 }
--- app/src/main/java/com/example/projectnam/ReserveLongActivity.java
+++ app/src/main/java/com/example/projectnam/ReserveLongActivity.java
@@ -1,11 +1,13 @@
 package com.example.projectnam;
 
+import static android.view.View.GONE;
 import static android.view.View.INVISIBLE;
 import static android.view.View.VISIBLE;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
@@ -14,6 +16,7 @@ import android.widget.DatePicker;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.util.Date;
@@ -23,8 +26,9 @@ public class ReserveLongActivity extends AppCompatActivity {
     CalendarView calendar;
     RelativeLayout relativeCalendar;
     TextView text1;
-    long now = System.currentTimeMillis();  // 현재시간을 불러오는 변수
+    long tim, now = System.currentTimeMillis();  // 현재시간을 불러오는 변수
 
+    CalendarDate calendarDate = new CalendarDate();
     int setTextLoca=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,22 +42,35 @@ public class ReserveLongActivity extends AppCompatActivity {
         relativeCalendar = (RelativeLayout)findViewById(R.id.relativeCalendar);
         text1 = (TextView)findViewById(R.id.text1);
 
-        CalendarDate calendarDate = new CalendarDate();
+        calendarDate.setCalendar(calendar);
 
-        restartBtn1.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v) {
-                setTextLoca=1;
-                relativeCalendar.setVisibility(VISIBLE);
-                text1.setVisibility(INVISIBLE);
-            }
-        });
         restartBtn2.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
                 setTextLoca=2;
                 relativeCalendar.setVisibility(VISIBLE);
+                calendar.setVisibility(VISIBLE);
                 text1.setVisibility(INVISIBLE);
+                calendarDate.setOffset(setTextLoca);
+                calendarDate.setMax();
+                calendarDate.setMin();
+                try {
+                    calendar.setDate(new SimpleDateFormat().parse(Integer.toString(calendarDate.finishYear) + "-" + Integer.toString(calendarDate.finishMonth) + "-" + Integer.toString(calendarDate.finishDay) + " 01:12:34").getTime());
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        restartBtn1.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                setTextLoca=1;
+                relativeCalendar.setVisibility(VISIBLE);
+                calendar.setVisibility(VISIBLE);
+                text1.setVisibility(INVISIBLE);
+                calendarDate.setOffset(setTextLoca);
+                calendarDate.setMax();
+                calendarDate.setMin();
             }
         });
 
@@ -61,13 +78,19 @@ public class ReserveLongActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 relativeCalendar.setVisibility(INVISIBLE);
+                calendar.setVisibility(GONE);
                 text1.setVisibility(VISIBLE);
-                if(setTextLoca==1) restartBtn1.setText(calendarDate.Year+"년" + calendarDate.Month+ "월" + calendarDate.Day + "일");
-                else restartBtn2.setText(calendarDate.Year+"년" + calendarDate.Month+ "월" + calendarDate.Day + "일");
+                if(setTextLoca==1){
+                    restartBtn1.setText(calendarDate.Year+"년" + calendarDate.Month+ "월" + calendarDate.Day + "일");
+                    calendarDate.saveStart();
+                }
+                else {
+                    restartBtn2.setText(calendarDate.Year+"년" + calendarDate.Month+ "월" + calendarDate.Day + "일");
+                    calendarDate.saveFinish();
+                }
             }
         });
         calendar.setOnDateChangeListener(calendarDate);
-        calendar.setMinDate(now);
         chooseDayBtn.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
