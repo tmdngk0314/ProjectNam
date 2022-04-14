c79ee1c2b13931b662fa9dea63b88dabc62a40f9
Lee dong hee <tmdngk0314@naver.com>
2022-04-14 19:40:04 +0900

    [ADD] 쓰레드사용으로 otp와 남은시간 실시간으로 출력

--- app/src/main/java/com/example/projectnam/OtpActivity.java
+++ app/src/main/java/com/example/projectnam/OtpActivity.java
@@ -10,6 +10,9 @@ import android.widget.TextView;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 public class OtpActivity extends AppCompatActivity {
     long otp = System.currentTimeMillis();
@@ -17,6 +20,7 @@ public class OtpActivity extends AppCompatActivity {
     String cursec,cursec2;
     SharedPreferences deviceInfo;
     TextView cursecondtext,otptext;
+    public ScheduledExecutorService exeService;
 
 
     @Override
@@ -28,25 +32,46 @@ public class OtpActivity extends AppCompatActivity {
         otptext = (TextView)findViewById(R.id.otptext);
 
 
-        //Calendar 클래스의 getTime()함수 사용
-        Calendar calendar = Calendar.getInstance();
-        curSecond = calendar.get(Calendar.SECOND); // 현재 시간의 초를 불러옴
+        Runnable runn = new Runnable() {
+            @Override
+            public void run() {
+                Calendar calendar = Calendar.getInstance();
+                curSecond = calendar.get(Calendar.SECOND);
+                String otp = ManageOTP.getCurrentOTP(CurrentLoggedInID.ID, deviceInfo);
+                //1초마다 동작시킬 코드
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                otptext.setText(otp);
+                                if(curSecond>0 && curSecond<=30) {
+                                    cursec = Integer.toString(30-curSecond);
+                                    cursecondtext.setText(cursec);}
+                                else if(curSecond>30 && curSecond<=60){
+                                    cursec2 = Integer.toString(60-curSecond);
+                                    cursecondtext.setText(cursec2);
+                                }  //1초마다 동작시킬 ui코드
+                            }
+                        });
+                    }
+                }).start();
+            }
+        };
+        // 1초마다 실행하기 시작
+        exeService= Executors.newSingleThreadScheduledExecutor();
+        exeService.scheduleAtFixedRate(runn, 0,1, TimeUnit.SECONDS);
 
-        if(curSecond>0 && curSecond<=30) {
-            cursec = Integer.toString(30-curSecond);
-            cursecondtext.setText(cursec);}
-        else if(curSecond>30 && curSecond<=60){
-            cursec2 = Integer.toString(60-curSecond);
-            cursecondtext.setText(cursec2);
 
-        }
+        //onDestroy 오버라이드해서 이거 꼭 첫줄에 넣어야함
 
 
-        String otp = ManageOTP.getCurrentOTP(CurrentLoggedInID.ID, deviceInfo);
-        otptext.setText(otp);
+    }
 
-
-
-
+    @Override
+    protected void onDestroy() {
+        exeService.shutdownNow();
+        super.onDestroy();
     }
 }
