e64b457662f1000996a50d231adc25340aaf0ca1
동희 <tmdngk0314>
2022-04-15 20:56:45 +0900

    [ADD] 예약정보확인 버튼 intent추가

--- app/src/main/java/com/example/projectnam/FixInfoActivity.java
+++ app/src/main/java/com/example/projectnam/FixInfoActivity.java
@@ -2,13 +2,28 @@ package com.example.projectnam;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class FixInfoActivity extends AppCompatActivity {
+    ImageButton reservecheckbtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fix_info);
+
+        reservecheckbtn = (ImageButton)findViewById(R.id.reservecheckbtn);
+
+
+        reservecheckbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(FixInfoActivity.this, ReInfoCheckActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
