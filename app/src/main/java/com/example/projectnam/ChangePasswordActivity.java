a7edb416f6bbb48e89dfd016d39511023e84c7c5
Lee dong hee <tmdngk0314@naver.com>
2022-04-21 19:44:38 +0900

    [ADD] 새 비밀번호와 새 비밀번호확인 비교하는 코드 작성

--- app/src/main/java/com/example/projectnam/ChangePasswordActivity.java
+++ app/src/main/java/com/example/projectnam/ChangePasswordActivity.java
@@ -8,12 +8,14 @@ import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 public class ChangePasswordActivity extends AppCompatActivity {
 
     EditText currentPass_Edt,newPass_Edt,newPassCheck_Edt;
-    TextView newPasstext;
+    TextView newPasstext,newPasschecktxt;
+    RelativeLayout Repass,Renewpass,Renewpasscheck;
     SharedPreferences deviceInfo;
     boolean isAvailable_pw(String str){
         if(str.length()>16 || str.length()<8){
@@ -38,6 +40,10 @@ public class ChangePasswordActivity extends AppCompatActivity {
         newPass_Edt = (EditText) findViewById(R.id.newPass_edt);
         newPassCheck_Edt = (EditText) findViewById(R.id.newpasscheck_edt);
         newPasstext = (TextView)findViewById(R.id.newPass_text);
+        newPasschecktxt = (TextView)findViewById(R.id.newPasscheck_text);
+        Repass = (RelativeLayout)findViewById(R.id.RePass);
+        Renewpass = (RelativeLayout)findViewById(R.id.ChangePass);
+        Renewpasscheck = (RelativeLayout)findViewById(R.id.newPassCheck);
 
         CallRestApi restApi = new CallRestApi();
 
@@ -55,12 +61,43 @@ public class ChangePasswordActivity extends AppCompatActivity {
             @Override
             public void afterTextChanged(Editable editable) {
                 String txt = editable.toString();
-                int length = txt.length();
-                String pwchk=newPassCheck_Edt.getText().toString();
                 if(isAvailable_pw(txt)==false){
                     newPasstext.setVisibility(View.VISIBLE);
+                    Renewpass.setBackgroundResource(R.drawable.border_red);
                 }
-                else newPasstext.setVisibility(View.INVISIBLE);
+                else {
+                    newPasstext.setVisibility(View.INVISIBLE);
+                    Renewpass.setBackgroundResource(R.drawable.border_gray);
+                }
+
+            }
+        });
+        newPassCheck_Edt.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                String txt = editable.toString();
+                String newPasstxt = newPass_Edt.getText().toString();
+                int length = newPasstxt.length();
+
+                if(txt.compareTo(newPasstxt)!=0){
+                    newPasschecktxt.setVisibility(View.VISIBLE);
+                    Renewpasscheck.setBackgroundResource(R.drawable.border_red);
+                }
+                else{
+                    newPasschecktxt.setVisibility(View.INVISIBLE);
+                    Renewpasscheck.setBackgroundResource(R.drawable.border_gray);
+
+                }
 
             }
         });
