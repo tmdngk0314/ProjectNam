a962a842a6dffd2c00befa270f188e5d35ced6d7
Lee dong hee <tmdngk0314@naver.com>
2022-05-02 17:09:39 +0900

    [ADD] 내 정보에 있는 버튼들 눌림 상태를 나타내줌

--- app/src/main/java/com/example/projectnam/FixInfoActivity.java
+++ app/src/main/java/com/example/projectnam/FixInfoActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
@@ -11,7 +12,7 @@ import android.widget.TextView;
 import org.w3c.dom.Text;
 
 public class FixInfoActivity extends AppCompatActivity {
-    RelativeLayout ChangePWrela,AccountdelRela;
+    RelativeLayout ChangePWrela,AccountdelRela,OTPReissurela;
     TextView textname;
     TextView textemail;
     @Override
@@ -32,6 +33,7 @@ public class FixInfoActivity extends AppCompatActivity {
         textemail.setText(CurrentLoggedInID.email);
         ChangePWrela = (RelativeLayout)findViewById(R.id.ChangePWrela);
         AccountdelRela = (RelativeLayout)findViewById(R.id.AccountDelrela);
+        OTPReissurela = (RelativeLayout)findViewById(R.id.OTPReissurela);
 
 
         ChangePWrela.setOnClickListener(new View.OnClickListener() {
@@ -41,12 +43,56 @@ public class FixInfoActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+        ChangePWrela.setOnTouchListener(new View.OnTouchListener(){
+            public boolean onTouch(View view, MotionEvent event){
+                switch(event.getAction()){
+                    case MotionEvent.ACTION_DOWN:
+                        ChangePWrela.setBackgroundResource(R.drawable.select_box_touch);
+                        return false;
+                    case MotionEvent.ACTION_UP:
+                        ChangePWrela.setBackgroundResource(R.drawable.select_box);
+                        return false;
+                    default: return false;
+                }
+
+            }
+        });
+
+        OTPReissurela.setOnTouchListener(new View.OnTouchListener(){
+            public boolean onTouch(View view, MotionEvent event){
+                switch(event.getAction()){
+                    case MotionEvent.ACTION_DOWN:
+                        OTPReissurela.setBackgroundResource(R.drawable.select_box_touch);
+                        return false;
+                    case MotionEvent.ACTION_UP:
+                        OTPReissurela.setBackgroundResource(R.drawable.select_box);
+                        return false;
+                    default: return false;
+                }
+
+            }
+        });
 
         AccountdelRela.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(FixInfoActivity.this, delaccount_pop_up_Activity.class);
                 startActivity(intent);
+            }
+        });
+
+        AccountdelRela.setOnTouchListener(new View.OnTouchListener(){
+            public boolean onTouch(View view, MotionEvent event){
+                switch(event.getAction()){
+                    case MotionEvent.ACTION_DOWN:
+                        AccountdelRela.setBackgroundResource(R.drawable.select_box_touch);
+                        return false;
+                    case MotionEvent.ACTION_UP:
+                        AccountdelRela.setBackgroundResource(R.drawable.select_box);
+                        return false;
+                    default: return false;
+                }
+
             }
         });
     }
