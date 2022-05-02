6429ddff9f48c641385eb9103b543aec4eda58da
Lee dong hee <tmdngk0314@naver.com>
2022-05-02 21:35:30 +0900

    [ADD] selectactivity 중복 코드 메소드화해서 코드 줄임

--- app/src/main/java/com/example/projectnam/SelectActivity.java
+++ app/src/main/java/com/example/projectnam/SelectActivity.java
@@ -5,9 +5,9 @@ import androidx.core.app.ActivityCompat;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.nfc.NfcAdapter;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
@@ -18,7 +18,7 @@ import android.widget.Toast;
 public class SelectActivity extends AppCompatActivity {
     public static Activity menu_activity;
     public static Boolean isLoggedOutByButton=false;
-    ImageButton imgBtnLogout, imgBtnreserve;
+    ImageButton imgBtnLogout, imgBtnreserve, imgBtnotpcheck, imgBtnmyinfo, imgBtnnotice;
     RelativeLayout myInfoRela, reserveRela, OTPRela, noticeRela;
     NfcAdapter mNfcAdapter;
 
@@ -42,12 +42,58 @@ public class SelectActivity extends AppCompatActivity {
         }
     }
 
+    public boolean onTouchReserve(MotionEvent event){
+        switch(event.getAction()){
+            case MotionEvent.ACTION_DOWN:
+                reserveRela.setBackgroundResource(R.drawable.select_box_touch);
+                return false;
+            case MotionEvent.ACTION_UP:
+                reserveRela.setBackgroundResource(R.drawable.select_box);
+                return false;
+            default: return false;
+        }
+    }
+    public void onClickReserve(View v){
+        CallRestApi apiCaller = new CallRestApi();
+        ReservationStatus status=new ReservationStatus();
+        status=apiCaller.checkReservationStatus();
+        Intent intent = new Intent(SelectActivity.this, LockerListActivity.class);
+        Intent intent2 = new Intent(SelectActivity.this,ReserveStateActivity.class);
+        Log.e("test", status.result);
+        if(status.result.equals("diffIP")){
+            Log.e("Login Session", "다른 기기에서 로그인되었음" );
+            Toast.makeText(SelectActivity.this, "다른 기기에서 로그인되어 종료합니다.", Toast.LENGTH_SHORT).show();
+            apiCaller.logout();
+            ActivityCompat.finishAffinity(SelectActivity.this);
+            System.exit(0);
+        }
+        if(status.result.equals("idle")) {
+            startActivity(intent);
+        }
+        else if(status.result.equals("reserved") || status.result.equals("using") || status.result.equals("overdue")){
+            intent2.putExtra("result", status.result);
+            intent2.putExtra("startdate", status.startdate);
+            intent2.putExtra("enddate", status.enddate);
+            intent2.putExtra("usinglockername", status.usinglockername);
+            intent2.putExtra("location", status.location);
+            if(status.result.equals("using"))
+                intent2.putExtra("lockernum", status.lockernum);
+            startActivity(intent2);
+        }
+        else
+            Toast.makeText(SelectActivity.this, "unkown statement", Toast.LENGTH_SHORT).show();
+
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select);
         imgBtnLogout = (ImageButton)findViewById(R.id.imgBtnLogout);
-        imgBtnreserve =(ImageButton)findViewById(R.id.storeimg) ;
+        imgBtnreserve =(ImageButton)findViewById(R.id.img_reserve) ;
+        imgBtnotpcheck = (ImageButton)findViewById(R.id.img_otpcheck);
+        imgBtnmyinfo = (ImageButton)findViewById(R.id.img_myinfo);
+        imgBtnnotice = (ImageButton)findViewById(R.id.img_notice);
         myInfoRela = (RelativeLayout)findViewById(R.id.thirdRela);
         reserveRela = (RelativeLayout)findViewById(R.id.firstRela);
         OTPRela = (RelativeLayout)findViewById(R.id.secondRela);
@@ -59,53 +105,27 @@ public class SelectActivity extends AppCompatActivity {
         startService(new Intent(this, ForcedTerminationService.class)); // 앱 강제종료 시 로그아웃하는 서비스
 
 
-
+        imgBtnreserve.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onClickReserve(v);
+            }
+        });
         reserveRela.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                CallRestApi apiCaller = new CallRestApi();
-                ReservationStatus status=new ReservationStatus();
-                status=apiCaller.checkReservationStatus();
-                Intent intent = new Intent(SelectActivity.this, LockerListActivity.class);
-                Intent intent2 = new Intent(SelectActivity.this,ReserveStateActivity.class);
-                Log.e("test", status.result);
-                if(status.result.equals("diffIP")){
-                    Log.e("Login Session", "다른 기기에서 로그인되었음" );
-                    Toast.makeText(SelectActivity.this, "다른 기기에서 로그인되어 종료합니다.", Toast.LENGTH_SHORT).show();
-                    apiCaller.logout();
-                    ActivityCompat.finishAffinity(SelectActivity.this);
-                    System.exit(0);
-                }
-                if(status.result.equals("idle")) {
-                    startActivity(intent);
-                }
-                else if(status.result.equals("reserved") || status.result.equals("using") || status.result.equals("overdue")){
-                    intent2.putExtra("result", status.result);
-                    intent2.putExtra("startdate", status.startdate);
-                    intent2.putExtra("enddate", status.enddate);
-                    intent2.putExtra("usinglockername", status.usinglockername);
-                    intent2.putExtra("location", status.location);
-                    if(status.result.equals("using"))
-                        intent2.putExtra("lockernum", status.lockernum);
-                    startActivity(intent2);
-                }
-                else
-                    Toast.makeText(SelectActivity.this, "unkown statement", Toast.LENGTH_SHORT).show();
-
+               onClickReserve(v);
+            }
+        });
+        imgBtnreserve.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                return onTouchReserve(event);
             }
         });
         reserveRela.setOnTouchListener(new View.OnTouchListener(){
             public boolean onTouch(View view, MotionEvent event){
-                switch(event.getAction()){
-                    case MotionEvent.ACTION_DOWN:
-                        reserveRela.setBackgroundResource(R.drawable.select_box_touch);
-                        return false;
-                    case MotionEvent.ACTION_UP:
-                        reserveRela.setBackgroundResource(R.drawable.select_box);
-                        return false;
-                    default: return false;
-                }
-
+                return onTouchReserve(event);
             }
         });
         OTPRela.setOnClickListener(new View.OnClickListener() {
@@ -120,6 +140,18 @@ public class SelectActivity extends AppCompatActivity {
 
                 else startActivity(intent1);
 
+            }
+        });
+        imgBtnotpcheck.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SelectActivity.this, otp_popup_Activity.class);
+                Intent intent1 = new Intent(SelectActivity.this, OtpActivity.class);
+
+                if(!mNfcAdapter.isEnabled()) {
+                    startActivity(intent);
+                }
+                else startActivity(intent1);
             }
         });
         imgBtnLogout.setOnClickListener(new View.OnClickListener(){
@@ -137,15 +169,7 @@ public class SelectActivity extends AppCompatActivity {
         });
         imgBtnLogout.setOnTouchListener(new View.OnTouchListener(){
             public boolean onTouch(View view, MotionEvent event){
-                switch(event.getAction()){
-                    case MotionEvent.ACTION_DOWN:
-                        imgBtnLogout.setBackgroundResource(R.drawable.select_logout_touch);
-                        return false;
-                    case MotionEvent.ACTION_UP:
-                        imgBtnLogout.setBackgroundResource(R.drawable.select_logout);
-                        return false;
-                    default: return false;
-                }
+               return onTouchReserve(event);
 
             }
         });
@@ -155,17 +179,21 @@ public class SelectActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+        imgBtnnotice.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view){
+                Intent intent = new Intent(SelectActivity.this, NoticeActivity.class);
+                startActivity(intent);
+            }
+        });
         noticeRela.setOnTouchListener(new View.OnTouchListener(){
             public boolean onTouch(View view, MotionEvent event){
-                switch(event.getAction()){
-                    case MotionEvent.ACTION_DOWN:
-                        noticeRela.setBackgroundResource(R.drawable.select_box_touch);
-                        return false;
-                    case MotionEvent.ACTION_UP:
-                        noticeRela.setBackgroundResource(R.drawable.select_box);
-                        return false;
-                    default: return false;
-                }
+                return onTouchReserve(event);
+
+            }
+        });
+        imgBtnnotice.setOnTouchListener(new View.OnTouchListener(){
+            public boolean onTouch(View view, MotionEvent event){
+                return onTouchReserve(event);
 
             }
         });
@@ -176,35 +204,37 @@ public class SelectActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+        imgBtnmyinfo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SelectActivity.this,EmailVerificationActivity.class);
+                startActivity(intent);
+            }
+        });
         myInfoRela.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View view, MotionEvent motionEvent) {
-                switch(motionEvent.getAction()){
-                    case MotionEvent.ACTION_DOWN:
-                        myInfoRela.setBackgroundResource(R.drawable.select_box_touch);
-                        return false;
-                    case MotionEvent.ACTION_UP:
-                        myInfoRela.setBackgroundResource(R.drawable.select_box);
-                        return false;
-                    default: return false;
-                }
+               return onTouchReserve(motionEvent);
+            }
+        });
+        imgBtnmyinfo.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                return onTouchReserve(event);
             }
         });
         OTPRela.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View view, MotionEvent motionEvent) {
-                switch(motionEvent.getAction()){
-                    case MotionEvent.ACTION_DOWN:
-                        OTPRela.setBackgroundResource(R.drawable.select_box_touch);
-                        return false;
-                    case MotionEvent.ACTION_UP:
-                        OTPRela.setBackgroundResource(R.drawable.select_box);
-                        return false;
-                    default: return false;
-                }
+                return onTouchReserve(motionEvent);
             }
         });
-
+        imgBtnotpcheck.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                return onTouchReserve(motionEvent);
+            }
+        });
 
     }
     @Override
