e856a1606b36bd4548d6a0b61f5c575d446ea714
Lee dong hee <tmdngk0314@naver.com>
2022-05-02 16:56:50 +0900

    [ADD] 회원탈퇴를 묻는 팝업창을 만듬

--- app/src/main/java/com/example/projectnam/FixInfoActivity.java
+++ app/src/main/java/com/example/projectnam/FixInfoActivity.java
@@ -11,7 +11,7 @@ import android.widget.TextView;
 import org.w3c.dom.Text;
 
 public class FixInfoActivity extends AppCompatActivity {
-    RelativeLayout ChangePWrela;
+    RelativeLayout ChangePWrela,AccountdelRela;
     TextView textname;
     TextView textemail;
     @Override
@@ -31,12 +31,21 @@ public class FixInfoActivity extends AppCompatActivity {
         textname.setText(CurrentLoggedInID.name);
         textemail.setText(CurrentLoggedInID.email);
         ChangePWrela = (RelativeLayout)findViewById(R.id.ChangePWrela);
+        AccountdelRela = (RelativeLayout)findViewById(R.id.AccountDelrela);
 
 
         ChangePWrela.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(FixInfoActivity.this, ChangePasswordActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        AccountdelRela.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(FixInfoActivity.this, delaccount_pop_up_Activity.class);
                 startActivity(intent);
             }
         });
