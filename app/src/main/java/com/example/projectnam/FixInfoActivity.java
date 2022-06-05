2910d71ce0f607a1af29727294d82f235af22371
Lee dong hee <tmdngk0314@naver.com>
2022-06-05 16:29:51 +0900

    [ADD] 1. 예약현황 액티비티 뒤로가기버튼       2. 내 정보 수정화면 뒤로가기버튼 구현       3. 사물함 리스트 뒤로가기 버튼 구현

--- app/src/main/java/com/example/projectnam/FixInfoActivity.java
+++ app/src/main/java/com/example/projectnam/FixInfoActivity.java
@@ -6,6 +6,7 @@ import androidx.core.app.ActivityCompat;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
@@ -22,7 +23,7 @@ public class FixInfoActivity extends AppCompatActivity {
     TextView textname;
     TextView textemail;
     SharedPreferences deviceInfo;
-    ImageButton im_delaccount, im_otpre, im_chagepw;
+    ImageButton im_delaccount, im_otpre, im_chagepw,goSelectAct;
 
     public boolean onTouchReserve(MotionEvent event, RelativeLayout a){
         switch(event.getAction()){
@@ -59,9 +60,17 @@ public class FixInfoActivity extends AppCompatActivity {
         im_chagepw = (ImageButton)findViewById(R.id.ChangePWimg);
         im_otpre = (ImageButton) findViewById(R.id.OTPReissuimg);
         im_delaccount = (ImageButton)findViewById(R.id.AccountDelimg);
+        goSelectAct = (ImageButton)findViewById(R.id.goSelectAct);
 
 
         deviceInfo=getSharedPreferences("accountOTP", 0);
+
+        goSelectAct.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
 
         ChangePWrela.setOnClickListener(new View.OnClickListener() {
             @Override
